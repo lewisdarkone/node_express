1984b6d37d615c0147f5b8896f76da93ef34a82a
lewis <lewis_rodriguez_b@claro.com.do>
2019-10-15 10:47:55 -0400

    cambio lamda to function

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) =>{
+app.get('/', function(req,res)  {
   res.send('Hello node');
 });
 
-app.listen(3000,() =>{
+app.listen(3000,() => {
   console.log('Server started in port 3000');
 });
